847a1bb11ee68f15cdcedc20ceca8b7e020d1336
Kokou KOUMAGNA <kokou.koumagnagottoh@gmail.com>
2020-10-05 22:41:49 +0200

    :recycle: core, data, view module

--- src/app/data/data.module.ts
+++ src/app/data/data.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {SharedModule} from '../shared/shared.module';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    SharedModule,
+    HttpClientModule
   ]
 })
-export class DataModule { }
+export class DataModule {
+}
--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -1,17 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TodoContainerComponent } from './todo-container/todo-container.component';
-import { TodoItemComponent } from './todo-item/todo-item.component';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TodoContainerComponent} from './todo-container/todo-container.component';
+import {TodoItemComponent} from './todo-item/todo-item.component';
+import {AddTodoItemFormContainerComponent} from './add-todo-item-form-container/add-todo-item-form-container.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DataModule} from '../data/data.module';
+import {CoreModule} from '../core/core.module';
 
 
 @NgModule({
-  declarations: [TodoContainerComponent, TodoItemComponent],
+  imports: [
+    CommonModule,
+    CoreModule,
+    DataModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    TodoContainerComponent,
+    TodoItemComponent,
+    AddTodoItemFormContainerComponent],
   exports: [
     TodoContainerComponent
-  ],
-  imports: [
-    CommonModule
   ]
 })
-export class ViewModule { }
+export class ViewModule {
+}
