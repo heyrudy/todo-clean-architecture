a534249dbc1171ddbcfe566680b42e65d7229553
Kokou KOUMAGNA <kokou.koumagnagottoh@gmail.com>
2020-10-05 22:47:05 +0200

    :sparkles: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {TodoContainerComponent} from './view/todo-container/todo-container.component';
+import {AboutComponent} from './layouts/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TodoContainerComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
