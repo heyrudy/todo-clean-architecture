fce7fbf94442de2d7fd4180dcfdcb15b189538b6
Kokou KOUMAGNA <kokou.koumagnagottoh@gmail.com>
2020-10-06 21:21:47 +0200

    :sparkles: add todo & delete todo

--- src/app/view/todo-container/todo-container.component.ts
+++ src/app/view/todo-container/todo-container.component.ts
@@ -1,7 +1,8 @@
-import {GetTodosUsecase} from '../../core/usecases/get-all-todos.usecase';
 import {Component, OnInit} from '@angular/core';
 import {TodoModel} from 'src/app/core/domain/todo.model';
-import {DeleteTodoUsecase} from '../../core/usecases/delete-todo-usecase.service';
+import {GetTodosUsecase} from '../../core/usecases/get-all-todos.usecase';
+import {CreateTodoUsecase} from '../../core/usecases/create-todo.usecase';
+import {DeleteTodoByIdUsecase} from '../../core/usecases/delete-todo-by-id-usecase';
 
 @Component({
   selector: 'hr-todo-container',
@@ -13,20 +14,33 @@ export class TodoContainerComponent implements OnInit {
   todos: Array<TodoModel>;
 
   constructor(private getTodosUsecase: GetTodosUsecase,
-              private deleteTodoByIdUsecase: DeleteTodoUsecase) {
+              private createTodoUsecase: CreateTodoUsecase,
+              private deleteTodoByIdUsecase: DeleteTodoByIdUsecase) {
   }
 
   ngOnInit(): void {
     this.getTodosUsecase.execute()
-      .subscribe(todos =>
+      .subscribe((todos: TodoModel[]) =>
         this.todos = todos);
   }
 
-  onDelete(todoToBeDeleted: TodoModel): void {
+  deleteTodo(todoToBeDeleted: TodoModel): void {
     // Delete on UI
-    this.todos = this.todos.filter(todo => todo !== todoToBeDeleted);
+    this.todos = this.todos
+      .filter((todo: TodoModel) =>
+        todo !== todoToBeDeleted);
 
     // Delete on Server
-    this.deleteTodoByIdUsecase.execute(todoToBeDeleted);
+    this.deleteTodoByIdUsecase.execute(todoToBeDeleted.id);
+  }
+
+  addTodo(newTodo: TodoModel): void{
+    this.createTodoUsecase
+      .execute(newTodo)
+      .subscribe(
+        (todo: TodoModel) => {
+          this.todos.push(todo);
+        }
+      );
   }
 }
